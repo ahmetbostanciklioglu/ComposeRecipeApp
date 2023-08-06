bcf0e964805049ff20393df6f68cb79016e43a10
Ahmet Bostancıklıoğlu <ahmetbostanciklioglu@192.168.1.5>
2023-08-06 15:14:11 +0300

    row layouts

--- app/src/main/java/com/ahmet/bostanciklioglu/composerecipeapp/MainActivity.kt
+++ app/src/main/java/com/ahmet/bostanciklioglu/composerecipeapp/MainActivity.kt
@@ -3,17 +3,29 @@ package com.ahmet.bostanciklioglu.composerecipeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ahmet.bostanciklioglu.composerecipeapp.ui.theme.ComposeRecipeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,22 +35,59 @@ class MainActivity : ComponentActivity() {
             ComposeRecipeAppTheme {
                 // A surface container using the 'background' color from the theme
 
+                val scrollState = rememberScrollState()
                 Column(
                     modifier = Modifier
-                        .width(200.dp)
-                        .height(200.dp)
-                        .border(border = BorderStroke(width = 1.dp, color = Color.Black))
+                        .fillMaxSize()
+                        .verticalScroll(scrollState)
+                        .background(color = Color(0xFFF2F2F2))
+
                 ) {
-                    Text(text = "ITEM 1", modifier = Modifier.align(Alignment.CenterHorizontally))
+                    Image(
+                        bitmap = ImageBitmap.imageResource(id = R.drawable.happy_meal_small),
+                        contentDescription = null,
+                        modifier = Modifier.height(300.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                text = "Happy Meal",
+                                style = TextStyle(fontSize = 26.sp)
+                            )
+                            Text(
+                                text = "$5.99",
+                                style = TextStyle(
+                                    color = Color(0xFF85bb65),
+                                    fontSize = 17.sp
+                                ),
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.padding(top = 10.dp))
+                        Text(
+                            text = "800 calories",
+                            style = TextStyle(fontSize = 17.sp)
+                        )
+                        Spacer(modifier = Modifier.padding(top = 10.dp))
+
+                        Button(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        ) {
+                            Text(text = "ORDER NOW", style = TextStyle(color = Color.White))
+                        }
+                    }
                 }
-                Row(
-                    modifier = Modifier
-                        .width(200.dp)
-                        .height(200.dp)
-                        .border(border = BorderStroke(width = 1.dp, color = Color.Black))
-                ) {
-                    Text(text = "ITEM 2", modifier = Modifier.align(Alignment.CenterVertically))
-                }
+
 
             }
         }
